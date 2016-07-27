9e4d46ac4ad90598449dde45912f74654ef79344
rajayogan <rajayogan27@gmail.com>
2016-07-27 18:18:56 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, trigger, state, style, transition, animate } from '@angular/core';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  animations: [
+    trigger('movementtrigger', [
+      state('firstpos', style({transform: 'translateY(0)'})),
+      state('secondpos', style({transform: 'translateY(25%)'})),
+      transition('firstpos => secondpos', [
+        animate('200ms ease-in')
+      ]),
+      transition('secondpos => firstpos', [
+        animate('200ms ease-out')
+      ])
+    ]),
+    trigger('showmsg', [
+      state('show', style({opacity: '1', height: '*'})),
+      state('hide', style({opacity: '0', height: '0px'})),
+      transition('show <=> hide', [
+        animate(1000)
+      ])
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'app works!';
+  state: string ='show';
+  
+  togglestates() {
+    this.state = (this.state === 'show' ? 'hide' : 'show');
+  }
 }
